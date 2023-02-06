a1778ddc29d83a965cc90d92826d86313b369136
Kureq <99142007+JKureq@users.noreply.github.com>
2023-02-06 20:45:46 +0000

    Second

--- services/panel/views.py
+++ services/panel/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'panel/index.html')
+
+def panel(request):
+    return render(request, 'panel/panel.html')
--- services/register/views.py
+++ services/register/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateUserForm
+from django.shortcuts import redirect
+from django.views.generic.edit import FormView
+from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
+
+
+def register(request):
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+        else:
+            return render(request, 'register/register.html',{
+                'form': form
+            })
+    else:
+        form = CreateUserForm()
+        return render(request, 'register/register.html', {
+            'form': form
+        })
+        
