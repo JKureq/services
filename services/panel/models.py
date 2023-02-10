a93bb0de0727ed724ad70efba6b3438c57ad6712
Kureq <99142007+JKureq@users.noreply.github.com>
2023-02-10 23:00:53 +0000

    .

--- services/panel/models.py
+++ services/panel/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+
+
+class Service(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+    date = models.DateField()
+    products = models.ManyToManyField(Product, related_name='service', blank=True)
